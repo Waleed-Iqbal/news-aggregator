133c750a8c615ffd8e8b48752c2a7dd7edb48c8b
Khawaja Waleed Iqbal <ikhawajawaleed@gmail.com>
2024-08-18 00:40:46 +0500

    create and user global styles

--- src/index.tsx
+++ src/index.tsx
@@ -5,17 +5,17 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-import "./index.scss";
+import "./global.scss";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function

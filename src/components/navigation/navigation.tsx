85747c6af6579d00d163bd4367707426838c65bf
Khawaja Waleed Iqbal <ikhawajawaleed@gmail.com>
2024-08-18 00:41:41 +0500

    rudimentary navigation

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,16 +1,25 @@
-import React from "react";
 import { Link } from "react-router-dom";
+
 import { AppNavigationLinks } from "../../utils/content";
+
+import Logo from "../../images/logo.svg";
+
+import "./navigation.scss";
+import "../../global.scss";
 
 export default function Navigation() {
   return (
-    <div>
-      <Link to={AppNavigationLinks.home}>Home</Link>&nbsp;
-      <Link to={AppNavigationLinks.settings}>Settings</Link>&nbsp;
-      <Link to={AppNavigationLinks.newsFeed}>News Feed</Link>&nbsp;
-      <br />
-      <br />
-      <br />
-    </div>
+    <section className="navigation-container">
+      <div className="logo-container">
+        <img src={Logo} alt="Logo" width={30} height={30} />
+        <h1>Streamline News</h1>
+      </div>
+
+      <nav className="navigation">
+        <Link to={AppNavigationLinks.home}>Home</Link>&nbsp;
+        <Link to={AppNavigationLinks.settings}>Settings</Link>&nbsp;
+        <Link to={AppNavigationLinks.newsFeed}>News Feed</Link>&nbsp;
+      </nav>
+    </section>
   );
 }

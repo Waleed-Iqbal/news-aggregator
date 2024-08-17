1bbfb8d5bc344c7580c01785e5aa38eb658dcf18
Khawaja Waleed Iqbal <ikhawajawaleed@gmail.com>
2024-08-17 17:55:29 +0500

    add content for pages

---
+++ src/scripts/content.ts
@@ -0,0 +1,33 @@
+import { IPageInfo } from "../interfaces";
+
+export enum AppNavigationLinks {
+  default = "/",
+  home = "/home",
+  settings = "/settings",
+  newsFeed = "/news-feed",
+}
+
+export enum PageTitles {
+  home = "Home",
+  settings = "Settings",
+  newsFeed = "News Feed",
+}
+
+export const PageInfo: IPageInfo = {
+  default: {
+    title: PageTitles.home,
+    link: AppNavigationLinks.home,
+  },
+  home: {
+    title: PageTitles.home,
+    link: AppNavigationLinks.home,
+  },
+  settings: {
+    title: PageTitles.settings,
+    link: AppNavigationLinks.settings,
+  },
+  newsFeed: {
+    title: PageTitles.newsFeed,
+    link: AppNavigationLinks.newsFeed,
+  },
+};

771b7b1c48214107ca566c543d6a4206d61d73d2
Khawaja Waleed Iqbal <ikhawajawaleed@gmail.com>
2024-08-18 03:16:25 +0500

    add nav functionality

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,25 +1,68 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
-import { AppNavigationLinks } from "../../utils/content";
+import { AppNavigationLinks, PageInfo } from "../../utils/content";
 
-import Logo from "../../images/logo.svg";
+import LOGO from "../../images/logo.svg";
+import ICON_NEWSPAPER from "../../images/newspaper.svg";
 
 import "./navigation.scss";
 import "../../global.scss";
 
 export default function Navigation() {
-  return (
-    <section className="navigation-container">
-      <div className="logo-container">
-        <img src={Logo} alt="Logo" width={30} height={30} />
-        <h1>Streamline News</h1>
-      </div>
+  const [isNavigationOpen, setIsNavigationOpen] = useState(false);
 
-      <nav className="navigation">
-        <Link to={AppNavigationLinks.home}>Home</Link>&nbsp;
-        <Link to={AppNavigationLinks.settings}>Settings</Link>&nbsp;
-        <Link to={AppNavigationLinks.newsFeed}>News Feed</Link>&nbsp;
-      </nav>
-    </section>
+  const toggleMobileNavigationMenu = () => {
+    setIsNavigationOpen(!isNavigationOpen);
+  };
+
+  const navigationLinks = Object.values(PageInfo).filter(
+    (pageInfo) => pageInfo.link !== AppNavigationLinks.default
+  );
+
+  return (
+    <>
+      <section className="navigation-container-desktop">
+        <Link className="logo-container" to={PageInfo.home.link}>
+          <img className="mr-8" src={LOGO} alt="Logo" width={30} height={30} />
+          <h2>Streamline News</h2>
+        </Link>
+
+        <nav className="navigation-links">
+          {navigationLinks.map((Page) => (
+            <Link key={Page.title} className="link" to={Page.link}>
+              {Page.title}
+            </Link>
+          ))}
+        </nav>
+      </section>
+      <section className="navigation-container-mobile">
+        <Link className="logo-container" to={PageInfo.home.link}>
+          <h2>Streamline News</h2>
+        </Link>
+
+        <img
+          alt="Close"
+          width={30}
+          height={30}
+          src={ICON_NEWSPAPER}
+          onClick={toggleMobileNavigationMenu}
+          className={`nav-menu-button ${isNavigationOpen ? "open" : "close"}`}
+        />
+
+        <nav className={`navigation-links ${isNavigationOpen ? "open" : ""}`}>
+          {navigationLinks.map((Page) => (
+            <Link
+              to={Page.link}
+              key={Page.title}
+              className="link"
+              onClick={toggleMobileNavigationMenu}
+            >
+              {Page.title}
+            </Link>
+          ))}
+        </nav>
+      </section>
+    </>
   );
 }

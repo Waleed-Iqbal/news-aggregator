64a3ff24dad8d95250e6bf8288acb604052c11d7
Khawaja Waleed Iqbal <ikhawajawaleed@gmail.com>
2024-08-18 17:47:08 +0500

    add grid layout for home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,16 +1,30 @@
 import { useContext, useEffect, useState } from "react";
 import { UserSettingsContext } from "../../utils/userSettings";
 
+import { newsAPIMockData, availableCategories } from "../../utils/content";
+import { NewsArticle } from "../../components/news-article/news-article";
+
 import { INewsAPIResponse, INewsArticle } from "../../utils/interfaces";
+
+import "./home.scss";
 
 export default function HomePage() {
   const userContext = useContext(UserSettingsContext);
-  const [newAPIHeadlines, setNewAPIHeadlines] = useState<INewsArticle[]>([]);
-  const [totalArticles, setTotalArticles] = useState<number>(0);
 
-  const getHeadlines = async () => {
+  // use these after testing
+  // const [newsAPIArticles, setNewAPIArticles] = useState<INewsArticle[]>([]);
+  // const [totalArticles, setTotalArticles] = useState<number>(0);
+
+  const [newsAPIArticles, setNewAPIArticles] = useState<INewsArticle[]>(
+    newsAPIMockData.articles
+  );
+  const [totalArticles, setTotalArticles] = useState<number>(
+    newsAPIMockData.totalResults
+  );
+
+  const setHeadlines = async () => {
     const response = await fetch(
-      "https://newsapi.org/v2/top-headlines?language=en",
+      "https://newsapi.org/v2/top-headlines?language=en&sortBy=popularity",
       {
         method: "GET",
         headers: {
@@ -20,23 +34,38 @@ export default function HomePage() {
     );
 
     const newsAPIResponse: INewsAPIResponse = await response.json();
-    setNewAPIHeadlines(newsAPIResponse.articles);
+    setNewAPIArticles(newsAPIResponse.articles);
     setTotalArticles(newsAPIResponse.totalResults);
   };
 
   // useEffect(() => {
-  //   getHeadlines();
+  //   setHeadlines();
   // }, []);
 
   return (
-    <>
+    <div className="mt-24 page-container">
       {/* to avoid excessive API calls */}
-      <button onClick={getHeadlines}>Set country to US</button>
-      <h1>This is home page</h1>
-      <p>Total results: {totalArticles === 0 ? "-loading-" : totalArticles}</p>
-      <pre>
-        <code>{JSON.stringify(setNewAPIHeadlines, null, 2)}</code>
-      </pre>
-    </>
+      {/* <p>Total results: {totalArticles === 0 ? "-loading-" : totalArticles}</p> */}
+
+      <section className="filters-container">
+        <div>Search by typing</div>
+        <div>Last x days/months/years</div>
+      </section>
+
+      <section className="news-articles-container">
+        {newsAPIArticles.map((article: INewsArticle, index: number) => (
+          <NewsArticle key={`${index}-${article.title}`} article={article} />
+        ))}
+      </section>
+
+      <section className="categories-container">
+        <h2>Categories</h2>
+        {availableCategories.newsAPI.map((category: string) => (
+          <li key={category} className="category">
+            {category}
+          </li>
+        ))}
+      </section>
+    </div>
   );
 }

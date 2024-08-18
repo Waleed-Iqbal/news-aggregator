03ed3709fb5728976dfe34aeb2f5a94dc7e82bf2
Khawaja Waleed Iqbal <ikhawajawaleed@gmail.com>
2024-08-19 02:31:13 +0500

    update settings page

--- src/pages/settings/settings.tsx
+++ src/pages/settings/settings.tsx
@@ -1,15 +1,101 @@
-import { useContext } from "react";
-import { UserSettingsContext } from "../../utils/userSettings";
+import { useContext, useState, MouseEvent, useEffect } from "react";
+import {
+  defaultUserSettings,
+  UserSettingsContext,
+} from "../../utils/userSettings";
+import { availableCategories, availableSources } from "../../utils/content";
+
+import "./settings.scss";
 
 export default function SettingsPage() {
   const userContext = useContext(UserSettingsContext);
 
+  const [selectedSources, setSelectedSources] = useState<string[]>(
+    defaultUserSettings.sources
+  );
+  const [selectedCategories, setSelectedCategories] = useState<string[]>(
+    defaultUserSettings.categories
+  );
+
+  useEffect(() => {
+    userContext.setUserSettings({
+      sources: selectedSources,
+      categories: selectedCategories,
+    });
+    // eslint-disable-next-line
+  }, [selectedSources, selectedCategories]);
+
+  const updateSelectedSources = (
+    e: MouseEvent<HTMLInputElement>,
+    source: string
+  ) => {
+    e.stopPropagation();
+
+    if (selectedSources.includes(source)) {
+      setSelectedSources(
+        selectedSources.filter((selectedSource) => selectedSource !== source)
+      );
+    } else {
+      setSelectedSources([...selectedSources, source]);
+    }
+  };
+
+  const updateSelectedCategories = (
+    e: MouseEvent<HTMLInputElement>,
+    category: string
+  ) => {
+    e.stopPropagation();
+
+    if (selectedCategories.includes(category)) {
+      setSelectedCategories(
+        selectedCategories.filter(
+          (selectedCategory) => selectedCategory !== category
+        )
+      );
+    } else {
+      setSelectedCategories([...selectedCategories, category]);
+    }
+  };
+
   return (
-    <>
-      <h1>This is settings page</h1>
-      <pre>
-        <code>{JSON.stringify(userContext, null, 2)}</code>
-      </pre>
-    </>
+    <section className="page-container settings-page">
+      <h1 className="page-heading">
+        Preferences (some are selected by default)
+      </h1>
+      <div className="categories-container">
+        <h2 className="category-heading">Sources</h2>
+        <div className="categories-list">
+          {availableSources.newsAPI.map((source: string) => (
+            <label key={source} className="label">
+              <input
+                value={source}
+                type="checkbox"
+                className="checkbox"
+                checked={selectedSources.includes(source)}
+                onClick={(e) => updateSelectedSources(e, source)}
+              />
+              {source}
+            </label>
+          ))}
+        </div>
+      </div>
+      <div className="categories-container">
+        <h2 className="category-heading">Categories</h2>
+        <div className="categories-list">
+          {availableCategories.newsAPI.map((category: string) => (
+            <label key={category} className="label">
+              <input
+                type="checkbox"
+                value={category}
+                className="checkbox"
+                checked={selectedCategories.includes(category)}
+                onClick={(e) => updateSelectedCategories(e, category)}
+              />
+              {category}
+            </label>
+          ))}
+        </div>
+      </div>
+    </section>
   );
 }

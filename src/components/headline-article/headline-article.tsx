e2a18e17d449879036f8771f01432869f0add0a4
Khawaja Waleed Iqbal <ikhawajawaleed@gmail.com>
2024-08-18 16:11:01 +0500

    add headline article component

---
+++ src/components/headline-article/headline-article.tsx
@@ -0,0 +1,10 @@
+import "./headline-article.scss";
+
+export const HeadlineArticle = () => {
+  return (
+    <article>
+      <h2>First News Article</h2>
+      <p>Content of the first news article</p>
+    </article>
+  );
+};

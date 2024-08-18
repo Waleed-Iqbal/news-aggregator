7903aef7ef6f1823e8476c59951be1207a467a33
Khawaja Waleed Iqbal <ikhawajawaleed@gmail.com>
2024-08-18 22:27:04 +0500

    add dates specific helper functions

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -10,3 +10,26 @@ export const getArticlePublishDate = (rawPublishDate: string): string => {
     .slice(0, 4)
     .join(" ");
 };
+
+/**
+ * Get the current date in the form "yyyy-mm-dd"
+ * @returns string - the formatted date string in the form "2024-08-17"
+ */
+export const getCurrentDate = (): string => {
+  return new Date().toISOString().split("T")[0];
+};
+
+/**
+ * Get the date in the form "yyyy-mm-dd" from N days ago
+ * @returns string - the formatted date string in the form "2024-08-17"
+ */
+export const getDateNDaysAgo = (days: number): string => {
+  const date = new Date();
+  date.setDate(date.getDate() - days);
+
+  const year: number = date.getFullYear();
+  const month: string = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-indexed
+  const day: string = String(date.getDate()).padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
+};
